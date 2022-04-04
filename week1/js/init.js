1627ca58ac49837277db6a72bc4a331d4817609d
ashley-lu <ashleyjlu@g.ucla.edu>
2022-04-04 15:16:25 -0700

    finished lab1

---
+++ week1/js/init.js
@@ -0,0 +1,22 @@
+console.log('Hello Asian Am 191! :)');
+
+// JavaScript const variable declaration
+const map = L.map('map').setView([34.0709, -118.444], 15);
+
+// Leaflet tile layer, i.e. the base map
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+//JavaScript let variable declaration to create a marker
+let marker = L.marker([34.0709, -118.444]).addTo(map)
+        .bindPopup('Math Sciences 4328 aka the Technology Sandbox<br> is the lab where I work in ')
+        .openPopup();
+
+let marker2 = L.marker([40.717979, -74.358749]).addTo(map)
+        .bindPopup('Where I\'m from!')
+        .openPopup();
